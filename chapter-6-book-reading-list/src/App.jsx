89a7fdcc7ef908c32cf428a41b073e52840612fe
João Pedro Lopes <JoaoPedro.Lopes.EXT@deco.proteste.pt>
2025-07-01 17:44:46 +0100

    feat: create edit form, implement toggle of form after edit and add book images

--- chapter-6-book-reading-list/src/App.jsx
+++ chapter-6-book-reading-list/src/App.jsx
@@ -9,6 +9,20 @@ const App = () => {
     setBooks([...books, { id: Math.round(Math.random() * 9999), title }])
   }
 
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle }
+      }
+
+      return book
+    })
+
+    setBooks(updatedBooks)
+  }
+
+  console.log(books)
+
   const deleteBookById = (id) => {
     const updatedBooks = books.filter((book) => {
       return book.id !== id
@@ -18,7 +32,8 @@ const App = () => {
   }
 
   return <div className='app'>
-    <BookList books={books} onDelete={deleteBookById}/>
+    <h1>Reading List</h1>
+    <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
     <BookCreate onCreate={createBook}/>
   </div>
 }
--- chapter-6-book-reading-list/src/components/BookShow.jsx
+++ chapter-6-book-reading-list/src/components/BookShow.jsx
@@ -1,8 +1,27 @@
-const BookShow = ({ book, onDelete }) => {
+import { useState } from 'react'
+import BookEdit from "./BookEdit" 
+
+const BookShow = ({ book, onDelete, onEdit }) => {
+  const [showEdit, setShowEdit] = useState(false)
+
+  const handleSubmit = (id, newTitle) => {
+    onEdit(id, newTitle)
+    setShowEdit(false)
+  }
+
   return <div className="book-show">
-    {book.title}
+    <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="books"/>
+    <div>
+      {!showEdit ?
+        <h3>{book.title}</h3> :
+        <BookEdit book={book} onSubmit={handleSubmit}/>
+      }
+    </div>
 
     <div className="actions">
+      <button className="edit" onClick={() => setShowEdit(!showEdit)}>
+        Edit
+      </button>
       <button className="delete" onClick={() => onDelete(book.id)}>
         Delete
       </button>
