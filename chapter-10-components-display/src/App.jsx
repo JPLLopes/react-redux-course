18418c46e9a900e1cf7e082743c6925b4d074191
João Pedro Lopes <JoaoPedro.Lopes.EXT@deco.proteste.pt>
2025-07-16 23:11:08 +0100

    create starter Accordion component implementation

--- chapter-10-components-display/src/App.jsx
+++ chapter-10-components-display/src/App.jsx
@@ -1,10 +1,31 @@
-import Button from "./components/Button";
-import ButtonPage from "./pages/ButtonPage.jsx";
-
+import ButtonPage from "./pages/ButtonPage";
+import Accordion from "./components/Accordion";
 const App = () => {
+  const items = [
+    {
+      key: 1,
+      label: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id dolor nec erat facilisis cursus.",
+    },
+    {
+      key: 2,
+      label: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id dolor nec erat facilisis cursus.",
+    },
+    {
+      key: 3,
+      label: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id dolor nec erat facilisis cursus.",
+    },
+  ];
+
   return (
     <div>
       <ButtonPage />
+      <Accordion items={items} />
     </div>
   );
 };
