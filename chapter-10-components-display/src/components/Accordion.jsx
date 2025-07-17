9f4377e46f833299b6a7df0d91c5afe9f16ef79c
João Pedro Lopes <JoaoPedro.Lopes.EXT@deco.proteste.pt>
2025-07-17 12:11:01 +0100

    add icon to each accordion, implement self closing and styling

--- chapter-10-components-display/src/components/Accordion.jsx
+++ chapter-10-components-display/src/components/Accordion.jsx
@@ -1,20 +1,44 @@
 import { useState } from "react";
-
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 const Accordion = ({ items }) => {
-  const [expandedItem, setExpandedItem] = useState(0);
+  const [expandedItem, setExpandedItem] = useState(-1);
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = expandedItem === index;
 
+    const handleClick = (newIndex) => {
+      setExpandedItem((current) => {
+        if (current === newIndex) {
+          return -1;
+        } else {
+          return newIndex;
+        }
+      });
+    };
+
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    );
+
     return (
       <div key={item.key}>
-        <div onClick={() => setExpandedItem(index)}>{item.label}</div>
-        {isExpanded && <div>{item.content}</div>}
+        <div
+          className={
+            "flex justify-between p-3 gray-50 border-b items-center cursor-pointer"
+          }
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
 
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 };
 
 export default Accordion;
