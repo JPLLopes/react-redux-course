2bd96642d05b7d43f22c93f24677bae1efa49716
João Pedro Lopes <JoaoPedro.Lopes.EXT@deco.proteste.pt>
2025-07-17 11:15:18 +0100

    implement accordion expansion and collapsing

--- chapter-10-components-display/src/components/Accordion.jsx
+++ chapter-10-components-display/src/components/Accordion.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const [expandedItem, setExpandedItem] = useState(0);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = expandedItem === index;
+
     return (
       <div key={item.key}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div onClick={() => setExpandedItem(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
