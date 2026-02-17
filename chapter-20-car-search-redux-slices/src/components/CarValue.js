39b0e3d8eac0ce55a098eaace55473f581be6859
João Pedro Lopes <JoaoPedro.Lopes.EXT@deco.proteste.pt>
2026-02-17 01:51:10 +0000

    feat: show total car value

--- chapter-20-car-search-redux-slices/src/components/CarValue.js
+++ chapter-20-car-search-redux-slices/src/components/CarValue.js
@@ -1,5 +1,21 @@
+import { useSelector } from "react-redux";
+import { createSelector } from "@reduxjs/toolkit";
+
+const memoizedTotalCost = createSelector(
+  [(state) => state.cars.data, (state) => state.cars.searchTerm],
+  (data, searchTerm) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+
+    return filteredCars.reduce((acc, car) => acc + car.cost, 0);
+  },
+);
+
 const CarValue = () => {
-  return <div>CarValue</div>;
+  const totalCost = useSelector(memoizedTotalCost);
+
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 };
 
 export default CarValue;
