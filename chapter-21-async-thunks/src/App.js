d01d398a0b7699982fc80463fc135cfe466eec34
João Pedro Lopes <JoaoPedro.Lopes.EXT@deco.proteste.pt>
2026-03-04 09:44:16 +0000

    feat: add initial boilerplate

---
+++ chapter-21-async-thunks/src/App.js
@@ -0,0 +1,11 @@
+import UsersList from "../src/components/UsersList";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <UsersList />
+    </div>
+  );
+}
+
+export default App;
