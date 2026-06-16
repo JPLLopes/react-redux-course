9d7c4bae555e782c3255c05a415b82b1059825c7
João Pedro Lopes <JoaoPedro.Lopes.EXT@deco.proteste.pt>
2026-06-16 16:51:58 +0100

    create and add ExpandablePanel component to UserList item

--- chapter-21-async-thunks/src/components/UsersListItem.jsx
+++ chapter-21-async-thunks/src/components/UsersListItem.jsx
@@ -3,28 +3,32 @@ import Button from "./Button";
 import useThunk from "../hooks/useThunk";
 import { GoTrashcan } from "react-icons/go";
 import { removeUser } from "../store";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 
 const UsersListItem = ({ user }) => {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
 
+  const header = (
+    <>
+      <Button
+        className="mr-3"
+        loading={isLoading}
+        onClick={() => doRemoveUser(user)}
+      >
+        <GoTrashcan />
+      </Button>
+
+      {error && <div>Error deleting user.</div>}
+
+      {user.name}
+    </>
+  );
+
   return (
-    <div key={user.id} className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button
-            className="mr-3"
-            loading={isLoading}
-            onClick={() => doRemoveUser(user)}
-          >
-            <GoTrashcan />
-          </Button>
-
-          {error && <div>Error deleting user.</div>}
-
-          {user.name}
-        </div>
-      </div>
-    </div>
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
   );
 };
 
